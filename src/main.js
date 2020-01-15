b5e30bf8e5d2169ab15de926d7e72efe943c072c
rokealvo <rokealvo@gmail.com>
2020-01-15 20:14:43 +0700

    add slider

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('hhhhhhhhhhhhhhhhhhhhh')
+import slick from 'slick-carousel'
+import $ from 'jquery'
+import "slick-carousel/slick/slick.css";
+
+(function main() {
+    $('.slider-wrapper').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+})();
